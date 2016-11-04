09f3b3bd3acd2e7d1e6b9911222872de8e77a297
Memory Yuan <memory78078@gmail.com>
2016-11-05 00:53:20 +0800

    slider animation v1

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
+	moduleId: module.id,
     selector: 'my-app',
-    template: `<h1>My First Angular App</h1>`
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+
+	@ViewChild('slider') slider: ElementRef;
+
+	readonly InitSliderStyle: any = {
+		opacity: 0,
+		zIndex: -100,
+		transform: `translate3d(0, 0, 0)`
+	}
+	private sliderStyle: any = this.InitSliderStyle;
+	private tableWidth = 672;
+	private sliderEle: HTMLDivElement;
+
+	ngAfterViewInit(){
+		this.sliderEle = this.slider.nativeElement;
+
+		this.sliderEle.addEventListener('transitionend', (e)=>{
+			console.log("end");
+			this.sliderStyle = this.InitSliderStyle;
+		}, false);
+	}
+
+	handlePan(e: any){
+		// console.log(e);
+		if(e.type === 'panstart'){
+			this.sliderStyle = {}
+		}else if(e.isFinal){
+			if(Math.abs(e.deltaX) >= this.tableWidth / 2 || Math.abs(e.velocity) > 2){
+				console.log("rl");
+				this.sliderStyle.transition = "transform 200ms ease-out";
+				let direction = e.deltaX > 0 ? 'right' : 'left';
+				if(direction === 'left'){
+					this.sliderStyle.transform = `translate3d(${-this.tableWidth}px, 0, 0)`;
+				}else{
+					this.sliderStyle.transform = `translate3d(${this.tableWidth}px, 0, 0)`;
+				}
+			}else{
+				console.log("center");
+				this.sliderStyle.transition = "transform 100ms ease-out";
+				this.sliderStyle.transform = `translate3d(0, 0, 0)`;
+			}
+		}else{
+			this.sliderStyle.transform = `translate3d(${e.deltaX}px, 0, 0)`;
+		}
+	}
+
+
+
+}
